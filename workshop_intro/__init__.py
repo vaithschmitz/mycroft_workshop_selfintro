6f7c10b4595806752e4dddcde0e58c7083df82db
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-06-11 15:53:50 +0100

    added basic script

--- workshop_intro/__init__.py
+++ workshop_intro/__init__.py
@@ -1,28 +1,25 @@
-# TODO: Add an appropriate license to your skill before publishing.  See
-# the LICENSE file for more information.
-
-# Below is the list of outside modules you'll be using in your skill.
-# They might be built-in to Python, from mycroft-core or from external
-# libraries.  If you use an external library, be sure to include it
-# in the requirements.txt file so the library is installed properly
-# when the skill gets installed later by a user.
-
 from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill, intent_handler
 from mycroft.util.log import LOG
 
-# Each skill is contained within its own class, which inherits base methods
-# from the MycroftSkill class.  You extend this class as shown below.
-
-# TODO: Change "Template" to a unique name for your skill
-class TemplateSkill(MycroftSkill):
+class WorkshopSelfIntroSkill(MycroftSkill):
 
     # The constructor of the skill, which calls MycroftSkill's constructor
     def __init__(self):
-        super(TemplateSkill, self).__init__(name="TemplateSkill")
+        super(WorkshopSelfIntroSkill, self).__init__(name="WorkshopSelfIntroSkill")
         
-        # Initialize working variables used within the skill.
-        self.count = 0
+
+    # hit API to get content on screen
+    @intent_handler(IntentBuilder("").require("Hello").require("Workshop"))
+    def handle_content_one(self, message):
+        url ="someurl/content1"
+        r=requests.get(url)
+        json_output=r.json()
+        output=json_output['data']
+        events=output["Events"]
+
+        self.speak_dialog(something something confirmation)
+
 
     # The "handle_xxxx_intent" function is triggered by Mycroft when the
     # skill's intent is matched.  The intent is defined by the IntentBuilder()
@@ -42,13 +39,6 @@ class TemplateSkill(MycroftSkill):
         #    dialogs/en-us/hello.world.dialog
         self.speak_dialog("hello.world")
 
-    @intent_handler(IntentBuilder("").require("Count").require("Dir"))
-    def handle_count_intent(self, message):
-        if message.data["Dir"] == "up":
-            self.count += 1
-        else:  # assume "down"
-            self.count -= 1
-        self.speak_dialog("count.is.now", data={"count": self.count})
 
     # The "stop" method defines what Mycroft does when told to stop during
     # the skill's execution. In this case, since the skill's functionality
@@ -62,4 +52,4 @@ class TemplateSkill(MycroftSkill):
 # The "create_skill()" method is used to create an instance of the skill.
 # Note that it's outside the class itself.
 def create_skill():
-    return TemplateSkill()
+    return WorkshopSelfIntroSkill()
