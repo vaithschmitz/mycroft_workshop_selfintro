d1f6c366bfcc47b1b9f89c15bc9bdd37ba947930
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-06-12 13:14:08 +0100

    logic routing for conditional nodes

--- workshop_intro/__init__.py
+++ workshop_intro/__init__.py
@@ -8,6 +8,10 @@ class WorkshopSelfIntroSkill(MycroftSkill):
     # The constructor of the skill, which calls MycroftSkill's constructor
     def __init__(self):
         super(WorkshopSelfIntroSkill, self).__init__(name="WorkshopSelfIntroSkill")
+
+        #   set state to avoid showing content multiple times
+        self.already_had_tour = False
+        self.already_had_background = False
         
 
     @intent_handler(IntentBuilder("").require("Hello")
@@ -29,22 +33,15 @@ class WorkshopSelfIntroSkill(MycroftSkill):
         events=output["Events"]
 
         # track content end and hit next intent
+        # if already had background, go straight to portfolio
+        if self.already_had_background:
+            handle_portfolio_intent()
+        else:
+            self.speak('background.or.portfolio')
         
 
 
     @intent_handler(IntentBuilder("").require("Background")
-        def handle_background_intent(self, message):
-            self.speak_dialog("confirm")
-
-            # get content from db
-            url ="someurl/content1"
-            r=requests.get(url)
-            json_output=r.json()
-            output=json_output['data']
-            events=output["Events"]
-        
-
-    @intent_handler(IntentBuilder("").require("Portfolio")
     def handle_background_intent(self, message):
         self.speak_dialog("confirm")
 
@@ -55,9 +52,28 @@ class WorkshopSelfIntroSkill(MycroftSkill):
         output=json_output['data']
         events=output["Events"]
 
+        # track content end and hit next intent
+        # if already had tour, go straight to portfolio
+        if self.already_had_tour:
+            handle_portfolio_intent()
+        else:
+            self.speak('tour.or.portfolio')
+        
+
+    @intent_handler(IntentBuilder("").require("Portfolio")
+    def handle_portfolio_intent(self, message):
+        self.speak_dialog("portfolio.confirm")
+
+        # get content from db
+        url ="someurl/content1"
+        r=requests.get(url)
+        json_output=r.json()
+        output=json_output['data']
+        events=output["Events"]
+
         # track content end, hit final message
 
-        
+
     
 
 
